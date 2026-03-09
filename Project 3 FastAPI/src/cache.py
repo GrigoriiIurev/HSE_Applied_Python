f4046eb17b97f37c679b199fcc568e9877a4abf0
Григорий Юрьев <grigorijurev@MacBook-Air-Grigorij.local>
2026-03-09 18:54:12 +0400

    Update cache for Render

--- Project 3 FastAPI/src/cache.py
+++ Project 3 FastAPI/src/cache.py
@@ -1,8 +1,27 @@
+# from redis import asyncio as aioredis
+# from config import REDIS_HOST, REDIS_PORT
+
+
+# redis = aioredis.from_url(
+#     f"redis://{REDIS_HOST}:{REDIS_PORT}",
+#     decode_responses=True
+# )
+
+
+import os
 from redis import asyncio as aioredis
-from config import REDIS_HOST, REDIS_PORT
 
+REDIS_URL = os.getenv("REDIS_URL")
 
-redis = aioredis.from_url(
-    f"redis://{REDIS_HOST}:{REDIS_PORT}",
-    decode_responses=True
-)
+if REDIS_URL:
+    redis = aioredis.from_url(
+        REDIS_URL,
+        decode_responses=True
+    )
+else:
+    from config import REDIS_HOST, REDIS_PORT
+
+    redis = aioredis.from_url(
+        f"redis://{REDIS_HOST}:{REDIS_PORT}",
+        decode_responses=True
+    )
