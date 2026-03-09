5f6b964b70c47094ad53ed68da18bdc4e0eae846
Григорий Юрьев <grigorijurev@MacBook-Air-Grigorij.local>
2026-03-09 16:41:25 +0400

    Added DATABASE_URL for Render

--- Project 3 FastAPI/src/database.py
+++ Project 3 FastAPI/src/database.py
@@ -9,10 +9,13 @@ from sqlalchemy.orm import declarative_base
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
+import os
 
-DATABASE_URL = (
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+# DATABASE_URL = (
+#     f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# )
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
